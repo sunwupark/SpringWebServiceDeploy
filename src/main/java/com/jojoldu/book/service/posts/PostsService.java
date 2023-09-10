75b76e28e3e951571268bc68f04beed3deff3976
SunwuPark <sunwu5678@gmail.com>
2023-09-10 15:51:31 +0900

    resources added

--- src/main/java/com/jojoldu/book/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.service.posts;
 
 import com.jojoldu.book.web.domain.posts.Posts;
 import com.jojoldu.book.web.domain.posts.PostsRepository;
+import com.jojoldu.book.web.dto.PostsListResponseDto;
 import com.jojoldu.book.web.dto.PostsResponseDto;
 import com.jojoldu.book.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(("해당 게시글이 없습니다. id=" + id)));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/jojoldu/book/web/IndexController.java
+++ src/main/java/com/jojoldu/book/web/IndexController.java
@@ -1,15 +1,20 @@
 package com.jojoldu.book.web;
 
+import com.jojoldu.book.service.posts.PostsService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
     @GetMapping("/posts/save")
