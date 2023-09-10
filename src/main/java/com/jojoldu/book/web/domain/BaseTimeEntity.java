3f2173db4463af875d0e8a4b3d013f0abac50d81
SunwuPark <sunwu5678@gmail.com>
2023-09-10 14:30:47 +0900

    adding BaseTimeEntity

--- src/main/java/com/jojoldu/book/web/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/book/web/domain/BaseTimeEntity.java
@@ -1,4 +1,23 @@
 package com.jojoldu.book.web.domain;
 
-public class BaseTimeEntity {
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+
 }
--- src/main/java/com/jojoldu/book/web/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/web/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.web.domain.posts;
 
+import com.jojoldu.book.web.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
