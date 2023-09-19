ba327b394b23f5c8683afc6603ca14681e2b4f53
SunwuPark <sunwu5678@gmail.com>
2023-09-19 19:55:01 +0900

    main, application.yml

--- src/main/java/com/jojoldu/book/Main.java
+++ src/main/java/com/jojoldu/book/Main.java
@@ -3,7 +3,6 @@ package com.jojoldu.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
