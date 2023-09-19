b9aed0d627de2c4f901b4d31830688512d3c57de
SunwuPark <sunwu5678@gmail.com>
2023-09-19 16:38:26 +0900

    main, application.yml

--- src/main/java/com/jojoldu/book/Main.java
+++ src/main/java/com/jojoldu/book/Main.java
@@ -2,11 +2,12 @@ package com.jojoldu.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
