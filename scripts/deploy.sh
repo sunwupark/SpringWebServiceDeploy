7845eea217ed70c28bada9384cdbaaa0e16b587e
SunwuPark <sunwu5678@gmail.com>
2023-09-20 17:07:48 +0900

    travis.yml, deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,7 +19,7 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar|grep jar|tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> Jar Name: $JAR_NAME"
 echo "> $JAR_NAME에 실행권한 추가"
@@ -27,6 +27,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.config.location=classpath:/application.yml,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
